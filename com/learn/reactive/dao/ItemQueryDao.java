87a7aa6fc3ff7ec871d8f5f744bf8d516b432e34
aniketsingh30 <aniket.javadeveloper@gmail.com>
2020-06-17 00:11:20 +0530

    Add files via upload

---
+++ com/learn/reactive/dao/ItemQueryDao.java
@@ -0,0 +1,32 @@
+package com.example.juint.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.juint.document.ItemDTO;
+import com.example.juint.repository.IItemQueryRepoitory;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class ItemQueryDao implements IItemQueryDao{
+	@Autowired
+	IItemQueryRepoitory itemQueryRepoitory;
+	public Mono<String> getHello() {
+		return Mono.just("Hello, Aniket!");
+	}
+
+	@Override
+	public Flux<ItemDTO> findAll() {
+		return itemQueryRepoitory.findAll();
+	}
+
+	@Override
+	public Mono<ItemDTO> findById(String id) {
+		return itemQueryRepoitory.findById(id);
+	}
+
+	@Override
+	public Mono<ItemDTO> findItemByDescription(String description) {
+		return itemQueryRepoitory.findItemByDescription(description);
+	}
+}
