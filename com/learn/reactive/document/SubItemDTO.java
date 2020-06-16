fffe2c7145c2771f4c11333e6f6e6ca78a7260db
aniketsingh30 <aniket.javadeveloper@gmail.com>
2020-06-17 00:13:34 +0530

    Create SubItemDTO.java

---
+++ com/learn/reactive/document/SubItemDTO.java
@@ -0,0 +1,14 @@
+package com.example.juint.document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SubItemDTO {
+	private String name;
+	private String value;
+
+}
